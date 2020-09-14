b6ce4f6484b0f28f62466768fb6852d4ce6ecb28
pcroland <pcroland@protonmail.ch>
2020-09-14 16:14:07 +0200

    change -loglevel warning to -v quiet

--- snippets.sh
+++ snippets.sh
@@ -130,7 +130,7 @@ EOF
 
   for x in "$@"; do
     b=$(basename "$x")
-    ffmpeg -y -loglevel warning -i "$x" -ac "$channel" spec_temp.w64
+    ffmpeg -y -v quiet -i "$x" -ac "$channel" spec_temp.w64
     sox spec_temp.w64 -n spectrogram -x 1776 -Y 1080 -o "${b%.*}.png"
     keksh "${b%.*}.png"
   done
@@ -476,7 +476,7 @@ EOF
 
   for i in "$@"; do
     b=$(basename "$i")
-    echo "ffmpeg -i '$i' -loglevel warning -ac ${channel} -f sox - | sox -p -S -b 24 '${b%.*}_as.${outformat}' ${soxmode} $factor $soxsample" >&2 | tee
+    echo "ffmpeg -i '$i' -v quiet -ac ${channel} -f sox - | sox -p -S -b 24 '${b%.*}_as.${outformat}' ${soxmode} $factor $soxsample" >&2 | tee
   done | parallel "${args[@]}"
 }
 
