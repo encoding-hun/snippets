a2a0c7112140661cc3a259c4355af897257b3f7e
pcroland <pcroland@protonmail.ch>
2020-11-02 20:27:46 +0100

    cheatsh

--- snippets.sh
+++ snippets.sh
@@ -664,3 +664,7 @@ winuptime() { uptime.exe | cut -c22-; }
 decode_pssh() {
   curl -s 'https://integration.widevine.com/_/pssh_decode' -H 'content-type: text/plain' --data-binary "${1:-$(cat)}" | tail +2 | jq
 }
+
+cheatsh() {
+  curl cheat.sh/"$1"
+}
