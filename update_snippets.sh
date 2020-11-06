263fc329be347310ab06548d736d5606f91d1a6d
nyuszika7h <nyuszika7h@gmail.com>
2020-11-06 20:13:57 +0100

    Fix updater

--- update_snippets.sh
+++ update_snippets.sh
@@ -17,9 +17,9 @@ detect_shell() {
   fi
 }
 detect_shell
-printf 'Detected shell as %s\n\n' "$shell"
+printf 'Detected shell as %s\n\n' "$shell" >&2
 
-printf 'Updating snippets...\n'
+printf 'Updating snippets...\n' >&2
 tmpfile=$(mktemp "${TMPDIR:-/tmp}/snippets.XXXXXXXXXX")
 curl -fsSL https://raw.githubusercontent.com/nyuszika7h/snippets/master/snippets.sh -o "$tmpfile"
 
@@ -28,10 +28,10 @@ snippets_file="$HOME/.${shell}_snippets"
 
 needs_source=1
 
-printf '\n'
+printf '\n' >&2
 if [[ -f "$snippets_file" ]]; then
   if diff -q "$snippets_file" "$tmpfile" >/dev/null; then
-    printf '\e[32mAlready up to date\e[0m\n'
+    printf '\e[32mAlready up to date\e[0m\n' >&2
     needs_source=0
   else
     diff --color=always -u "$snippets_file" "$tmpfile"
@@ -39,32 +39,32 @@ if [[ -f "$snippets_file" ]]; then
 else
   diff --color=always -u "/dev/null" "$tmpfile"
 fi
-printf '\n'
+printf '\n' >&2
 
 if (( needs_source )); then
-  printf 'Saving snippets to %s\n' "$snippets_file"
+  printf 'Saving snippets to %s\n' "$snippets_file" >&2
   mv "$tmpfile" "$snippets_file"
 else
   rm -f "$tmpfile"
 fi
 
 if grep -q -F "$(basename "$snippets_file")" "$rcfile"; then
-  printf 'Found existing source line in %s\n' "$rcfile"
+  printf 'Found existing source line in %s\n' "$rcfile" >&2
 else
-  printf 'Adding source line to %s\n' "$rcfile"
+  printf 'Adding source line to %s\n' "$rcfile" >&2
   printf 'source %s\n' "$snippets_file" >> "$rcfile"
 fi
 
-printf '\nDone!'
+printf '\nDone!' >&2
 
 if (( needs_source )); then
-  if [[ $1 != '--selfupdate' ]]; then
-    printf ' Restart your shell or run:\n'
-    printf '$ \e[1msource %s\e[0m\n' "$rcfile"
+  if [[ $1 == '--selfupdate' ]]; then
+    printf '%s' "$rcfile"
+    printf '\n' >&2
   else
-    printf '\n'
+    printf ' Restart your shell or run:\n' >&2
+    printf '$ \e[1msource %s\e[0m\n' "$rcfile" >&2
   fi
 else
-  printf '\n'
-  exit 1
+  printf '\n' >&2
 fi
